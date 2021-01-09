ddb555c3169ff14557043cbb3ad6e60963c94d72
sam <sammybonfanti@gmail.com>
2021-01-08 20:51:30 -0700

    switching the active player  and class

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting elements
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0')
 const score1El = document.querySelector('#score--1')
 const current0El = document.querySelector('#current--0')
@@ -16,13 +18,14 @@ score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
 
+const scores = [0, 0]
 let currentScore = 0
+let activePlayer = 0
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', () => {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1
-    console.log("dice", dice)
 
     // 2. Display dice
     diceEl.classList.remove('hidden')
@@ -32,8 +35,13 @@ btnRoll.addEventListener('click', () => {
     if (dice !== 1) {
         // add dice to current score
         currentScore += dice
-        current0El.textContent = currentScore // CHANGE LATER
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore
     } else {
         // Switch to next player
+        document.querySelector(`#current--${activePlayer}`).textContent = 0
+        currentScore = 0
+        activePlayer = activePlayer === 0 ? 1 : 0
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')
     }
 })
